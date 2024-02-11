961bfc7c25ed9d0252eeb2f95946b909f2054a7f
Erik <eguntner2291@gmail.com>
2024-02-10 21:58:52 -0800

    Add theme toggle

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import * as Tooltip from "@radix-ui/react-tooltip";
+import * as RadixTooltip from "@radix-ui/react-tooltip";
 import {
   useMotionValue,
   motion,
@@ -13,8 +13,9 @@ import {
   EnvelopeClosedIcon,
   FileTextIcon,
   GitHubLogoIcon,
-  HomeIcon,
   LinkedInLogoIcon,
+  MoonIcon,
+  SunIcon,
 } from "@radix-ui/react-icons";
 
 import styles from "./footer.module.css";
@@ -43,7 +44,28 @@ export const footerLinks = [
 ];
 
 export const Footer = () => {
+  const [theme, setTheme] = React.useState<"light" | "dark">(() => {
+    const theme = localStorage.getItem("theme") || "light";
+    return theme;
+  });
+
   let mouseX = useMotionValue(Infinity);
+
+  const handleToggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  React.useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   return (
     <footer
@@ -62,21 +84,29 @@ export const Footer = () => {
           href={href}
         />
       ))}
+      <div className={styles.divider} />
+      <ToolTip label="Change theme">
+        <button
+          onClick={handleToggleTheme}
+          className={styles.link}
+          style={{ border: "none" }}
+        >
+          <div>{theme === "dark" ? <MoonIcon /> : <SunIcon />}</div>
+        </button>
+      </ToolTip>
     </footer>
   );
 };
 
-const AppIcon = ({
-  mouseX,
-  icon,
-  label,
-  href,
-}: {
+interface AppIconProps {
   mouseX: MotionValue;
   icon: React.JSX.Element;
   label: string;
   href: string;
-}) => {
+  type?: "a" | "button";
+}
+
+const AppIcon = ({ mouseX, icon, label, href, type = "a" }: AppIconProps) => {
   const ref = React.useRef<HTMLAnchorElement>(null);
   let distance = useTransform(mouseX, (val) => {
     let bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
@@ -84,8 +114,9 @@ const AppIcon = ({
   });
 
   let widthSync = useTransform(distance, [-200, 0, 200], [40, 70, 40]);
-  let svgWidthSync = useTransform(distance, [-200, 0, 200], [1, 1.5, 1]);
   let size = useSpring(widthSync, { stiffness: 200, damping: 15, mass: 0.1 });
+
+  let svgWidthSync = useTransform(distance, [-200, 0, 200], [1, 1.5, 1]);
   let svgSize = useSpring(svgWidthSync, {
     stiffness: 200,
     damping: 15,
@@ -93,26 +124,41 @@ const AppIcon = ({
   });
 
   return (
-    <Tooltip.Provider>
-      <Tooltip.Root delayDuration={200}>
-        <Tooltip.Trigger asChild>
-          <motion.a
-            ref={ref}
-            href={href}
-            target="_blank"
-            style={{ width: size, height: size }}
-            className={styles.link}
+    <ToolTip label={label}>
+      <motion.a
+        ref={ref}
+        href={href}
+        target="_blank"
+        style={{ width: size, height: size }}
+        className={styles.link}
+      >
+        <motion.div style={{ scale: svgSize }}>{icon}</motion.div>
+      </motion.a>
+    </ToolTip>
+  );
+};
+
+const ToolTip = ({
+  label,
+  children,
+}: {
+  label: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <RadixTooltip.Provider>
+      <RadixTooltip.Root delayDuration={200}>
+        <RadixTooltip.Trigger asChild>{children}</RadixTooltip.Trigger>
+        <RadixTooltip.Portal>
+          <RadixTooltip.Content
+            className={styles.TooltipContent}
+            sideOffset={5}
           >
-            <motion.div style={{ scale: svgSize }}>{icon}</motion.div>
-          </motion.a>
-        </Tooltip.Trigger>
-        <Tooltip.Portal>
-          <Tooltip.Content className={styles.TooltipContent} sideOffset={5}>
             {label}
-            <Tooltip.Arrow className={styles.TooltipArrow} />
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-    </Tooltip.Provider>
+            <RadixTooltip.Arrow className={styles.TooltipArrow} />
+          </RadixTooltip.Content>
+        </RadixTooltip.Portal>
+      </RadixTooltip.Root>
+    </RadixTooltip.Provider>
   );
 };
